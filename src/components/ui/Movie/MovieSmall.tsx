06a7671e8bc5f28ea4a25b2bbbb526db659a929f
andreikostsen <pks@tut.by>
2026-02-09 17:33:51 +0300

    MovieSmall component rev01

--- src/components/ui/Movie/MovieSmall.tsx
+++ src/components/ui/Movie/MovieSmall.tsx
@@ -1,10 +1,36 @@
+import { Box } from "@mui/material"
 import { Movie } from "../../../services/movie.types.ts"
 
-export const MovieSmall = (props: Movie) => {
+export const MovieSmall = ({ movie }: { movie: Movie }) => {
+
+  const imageUrl = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w342${movie.poster_path}`
+    : null;
 
   return (
-    <div>
-      small movie</div>
-  )
+    <Box
+      sx={{
+        cursor: "pointer",
+        transition: "transform .2s",
+        "&:hover": {
+          transform: "scale(1.05)",
+        },
+      }}
+    >
+      {imageUrl && (
+        <Box
+          component="img"
+          src={imageUrl}
+          sx={{
+            width: "100%",
+            borderRadius: 2,
+          }}
+        />
+      )}
 
+      <Box sx={{ mt: 1, fontSize: 14 }}>
+        {movie.title}
+      </Box>
+    </Box>
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,26 @@
 import { useGetPopularMoviesQuery, useGetWelcomePosterQuery } from "../services/tmdbApi.ts"
 import { LinearProgress, Box } from "@mui/material"
+import { MovieSmall } from "../components/ui/Movie/MovieSmall.tsx"
+import { Movie } from "../services/movie.types.ts"
 
 export const Home = () => {
   const { data, isLoading } = useGetWelcomePosterQuery()
 
   const { currentData } =useGetPopularMoviesQuery()
 
-  console.log(currentData)
+   console.log(currentData?.results.slice(0, 5))
+  // console.log(currentData)
 
   if (isLoading) return <LinearProgress />
 
   const imageUrl = `https://image.tmdb.org/t/p/original${data?.backdrop_path}`
 
   return (
+    <>
     <Box
       sx={{
         width: "100%",
-        height: "100%",
+        height: "80vh",
         backgroundImage: `
       linear-gradient(rgba(0,0,0,.4), rgba(0,0,0,.6)),
       url(${imageUrl})
@@ -28,5 +32,29 @@ export const Home = () => {
     >
       <h1>Welcome!</h1>
     </Box>
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: {
+            xs: "repeat(2, 1fr)",
+            sm: "repeat(3, 1fr)",
+            md: "repeat(6, 1fr)",
+          },
+          gap: 2,
+          p: 3,
+          maxWidth: 1320,
+          mx: "auto",
+        }}
+      >
+                {currentData?.results.slice(0, 6).map((item: Movie) => (
+                  <Box>
+                    <MovieSmall key={item.id} movie={item} />
+                  </Box>
+                ))}
+
+      </Box>
+
+
+    </>
   )
 }
